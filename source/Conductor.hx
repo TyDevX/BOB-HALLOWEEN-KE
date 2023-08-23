0fd5e123438ef1c9bb5e16cf27a93283462ea511
Frye <108774265+TyDevX@users.noreply.github.com>
2023-08-23 16:10:36 -0400

    charts should hopefully work now

--- source/Conductor.hx
+++ source/Conductor.hx
@@ -3,10 +3,6 @@ package;
 import Song.SongData;
 import flixel.FlxG;
 
-/**
- * ...
- * @author
- */
 typedef BPMChangeEvent =
 {
 	var stepTime:Int;
@@ -17,32 +13,26 @@ typedef BPMChangeEvent =
 class Conductor
 {
 	public static var bpm:Float = 100;
+
 	public static var crochet:Float = ((60 / bpm) * 1000); // beats in milliseconds
 	public static var stepCrochet:Float = crochet / 4; // steps in milliseconds
+
 	public static var songPosition:Float;
 	public static var lastSongPos:Float;
 	public static var offset:Float = 0;
 
-	public static var rawPosition:Float;
-
-	public static var safeFrames:Int = 10;
-	public static var safeZoneOffset:Float = Math.floor((safeFrames / 60) * 1000); // is calculated in create(), is safeFrames in milliseconds
-	public static var timeScale:Float = Conductor.safeZoneOffset / 166;
-
+	/*
+		public static var safeFrames:Int = 10;
+		public static var safeZoneOffset:Float = (safeFrames / 60) * 1000;
+	 */
 	public static var bpmChangeMap:Array<BPMChangeEvent> = [];
 
 	public function new()
 	{
+		//
 	}
 
-	public static function recalculateTimings()
-	{
-		Conductor.safeFrames = FlxG.save.data.frames;
-		Conductor.safeZoneOffset = Math.floor((Conductor.safeFrames / 60) * 1000);
-		Conductor.timeScale = Conductor.safeZoneOffset / 166;
-	}
-
-	public static function mapBPMChanges(song:SongData)
+	public static function mapBPMChanges(song:SwagSong)
 	{
 		bpmChangeMap = [];
 
@@ -66,30 +56,14 @@ class Conductor
 			totalSteps += deltaSteps;
 			totalPos += ((60 / curBPM) * 1000 / 4) * deltaSteps;
 		}
-		trace("new BPM map BUDDY " + bpmChangeMap);
+		// trace("new BPM map BUDDY " + bpmChangeMap);
 	}
 
-	public static function recalculateTimingStruct(SONG:SongData)
-	{
-		for (i in SONG.eventObjects)
-		{
-			/*TimingStruct.addTiming(beat,bpm,endBeat, Std.parseFloat(OFFSET));
-
-				if (changeEvents.length != 0)
-				{
-					var data = TimingStruct.AllTimings[currentIndex - 1];
-					data.endBeat = beat;
-					data.length = (data.endBeat - data.startBeat) / (data.bpm / 60);
-					TimingStruct.AllTimings[currentIndex].startTime = data.startTime + data.length;
-			}*/
-		}
-	}
-
-	public static function changeBPM(newBpm:Float, ?recalcLength = true)
+	public static function changeBPM(newBpm:Float, measure:Float = 4 / 4)
 	{
 		bpm = newBpm;
 
 		crochet = ((60 / bpm) * 1000);
-		stepCrochet = crochet / 4;
+		stepCrochet = (crochet / 4) * measure;
 	}
 }
